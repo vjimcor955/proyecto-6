e4bff9beecdde8091c474e37ee6f8d6278593793
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-05 11:38:43 +0100

    App complete

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <router-view />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+
 <script>
+  import Navbar from './components/shared/Navbar.vue'
+  import Footer from './components/shared/Footer.vue'
+
+  export default {
+    name: 'App',
+    components: { 
+      Navbar,
+      Footer
+    }
+  }
 
 </script>
 
 
-<template>
+<style scoped lang="scss">
+  main {
+    min-height: calc(100vh - 125px);
 
-</template>
-
-
-<style scoped>
-
+  }
 </style>
