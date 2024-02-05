40cf14f9287375c36383de982bb1e02388f8ecf6
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-05 10:08:06 +0100

    Footer component

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Music App © 2024 por Víctor Jiménez Corada 2º DAW A</p>
+  </footer>
+</template>
+
+
+<script>
+  export default {
+    
+  }
+</script>
+
+
+<style scoped lang="scss">
+  footer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    background-color: #f1f1f1;
+  }
+</style>
