20bd8c1474e7a572e3c1994ba083e7e8ae82a52b
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-05 10:07:16 +0100

    reset.scss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './components/router/router.js'
+import './assets/reset.scss'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
