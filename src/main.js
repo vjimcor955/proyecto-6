93a06e8eee729547f9bf1f7f9dd91b7886127239
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-07 09:34:01 +0100

    refactoring sass

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './components/router/router.js'
-import './assets/reset.scss'
+import './assets/main.scss'
 
 createApp(App)
 .use(router)
