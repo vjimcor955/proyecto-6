5b071704772da6d71e983bb43d0efe5a6b824e4a
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-15 18:52:47 +0100

    router refactored

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -2,15 +2,42 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../module/pages/Home.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/contact', component: () => import('../module/pages/Contact.vue') },
-  { path: '/login', component: () => import('../module/pages/LoginPage.vue') },
-  { path: '/general-ranking', component: () => import('../module/pages/GeneralRanking.vue') },
-  { path: '/lists', component: () => import('../module/pages/Lists.vue') },
-  { path: '/create-list', component: () => import('../module/pages/CreateList.vue') },
-  { path: '/user', component: () => import('../module/pages/User.vue') },
-  { path: '/create-element', component: () => import('../module/pages/CreateElement.vue') },
-  { path: '/:pathMatch(.*)*', component: () => import('../module/pages/NotFound404.vue') },
+  { path: '/', 
+    name: 'home', 
+    component: Home 
+  },
+  { path: '/contact', 
+    name: 'contact',
+    component: () => import('../module/pages/Contact.vue') 
+  },
+  { path: '/login', 
+    name: 'login',
+    component: () => import('../module/pages/LoginRegister.vue') 
+  },
+  { path: '/general-ranking', 
+    name: 'general-ranking',
+    component: () => import('../module/pages/GeneralRanking.vue') 
+  },
+  { path: '/lists', 
+    name: 'lists',
+    component: () => import('../module/pages/Lists.vue') 
+  },
+  { path: '/create-list', 
+    name: 'create-list',
+    component: () => import('../module/pages/CreateList.vue') 
+  },
+  { path: '/user', 
+    name: 'user',
+    component: () => import('../module/pages/User.vue') 
+  },
+  { path: '/create-element', 
+    name: 'create-element',
+    component: () => import('../module/pages/CreateElement.vue') 
+  },
+  { path: '/:pathMatch(.*)*', 
+    name: 'not-found',
+    component: () => import('../module/pages/NotFound404.vue') 
+  },
 ]
 
 const router = createRouter({
