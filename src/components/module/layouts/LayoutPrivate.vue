84cc0b609454a7eb4f2151ea996336ec369f8ca7
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-16 19:31:29 +0100

    layouts created

---
+++ src/components/module/layouts/LayoutPrivate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout_root">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import useAuthStore from '@/components/stores/authStore';
+
+  export default {
+    name: 'LayoutPrivate',
+    computed: {
+      isLogged() {
+        return useAuthStore().isLogged
+      }
+    },
+    // If user is not logged cant access to layoutPrivate, redirect to login page
+    created() {
+      if (!this.isLogged) {
+        this.$router.push('/login')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .layout_root {
+    min-height: calc(100vh - 125px);
+    font-family: "Afacad";
+  }
+</style>
---
+++ src/components/module/layouts/LayoutRoot.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout_root">
+    <router-view @login="login"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      login() {
+        this.$emit('login')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .layout_root {
+    min-height: calc(100vh - 125px);
+    font-family: "Afacad";
+  }
+</style>
