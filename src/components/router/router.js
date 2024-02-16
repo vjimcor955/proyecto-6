995279c00e57aeaaf937ceb2775e1aa6218800da
vjimcor955 <vjimcor955@g.educaand.es>
2024-02-16 09:17:53 +0100

    layouts routed

--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,38 +1,51 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../module/pages/Home.vue'
 
 const routes = [
-  { path: '/', 
-    name: 'home', 
-    component: Home 
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../module/layouts/LayoutRoot.vue"),
+    children: [
+      { path: '', 
+        name: 'root-home', 
+        component: () => import('../module/pages/Home.vue'), 
+      },
+      { path: '/contact', 
+        name: 'contact',
+        component: () => import('../module/pages/Contact.vue') 
+      },
+      { path: '/login', 
+        name: 'login',
+        component: () => import('../module/pages/LoginRegister.vue') 
+      },
+    ]
   },
-  { path: '/contact', 
-    name: 'contact',
-    component: () => import('../module/pages/Contact.vue') 
-  },
-  { path: '/login', 
-    name: 'login',
-    component: () => import('../module/pages/LoginRegister.vue') 
-  },
-  { path: '/general-ranking', 
-    name: 'general-ranking',
-    component: () => import('../module/pages/GeneralRanking.vue') 
-  },
-  { path: '/lists', 
-    name: 'lists',
-    component: () => import('../module/pages/Lists.vue') 
-  },
-  { path: '/create-list', 
-    name: 'create-list',
-    component: () => import('../module/pages/CreateList.vue') 
-  },
-  { path: '/user', 
-    name: 'user',
-    component: () => import('../module/pages/User.vue') 
-  },
-  { path: '/create-element', 
-    name: 'create-element',
-    component: () => import('../module/pages/CreateElement.vue') 
+  {
+    path: "/user",
+    name: "user",
+    component: () => import("../module/layouts/LayoutPrivate.vue"),
+    children: [
+      { path: '', 
+        name: 'private-user',
+        component: () => import('../module/pages/User.vue') 
+      },
+      { path: '/general-ranking', 
+        name: 'general-ranking',
+        component: () => import('../module/pages/GeneralRanking.vue') 
+      },
+      { path: '/lists', 
+        name: 'lists',
+        component: () => import('../module/pages/Lists.vue') 
+      },
+      { path: '/create-list', 
+        name: 'create-list',
+        component: () => import('../module/pages/CreateList.vue') 
+      },
+      { path: '/create-element', 
+        name: 'create-element',
+        component: () => import('../module/pages/CreateElement.vue') 
+      },
+    ]
   },
   { path: '/:pathMatch(.*)*', 
     name: 'not-found',
